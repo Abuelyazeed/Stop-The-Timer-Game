e14f2cb8aefa552ee527c5c4aef665055da750f9
Abuelyazeed <abuelyazeed175@gmail.com>
2024-06-26 13:02:45 +0300

    set Player naem and display it using state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [name, setName] = useState('');
+  const [displayedName, setDisplayedName] = useState('unknown entity');
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {displayedName}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" value={name} onChange={handleChange} />
+        <button onClick={() => setDisplayedName(name)}>Set Name</button>
       </p>
     </section>
   );
